07a2d4785f9f696cbf6fc54c5ef392a3c96e2eb0
atomic928 <64305950+atomic928@users.noreply.github.com>
2021-12-14 17:25:05 +0900

    fragmentからactivityへの遷移

--- app/src/main/java/com/example/smile_hackathon_product/ExerciseActivity.kt
+++ app/src/main/java/com/example/smile_hackathon_product/ExerciseActivity.kt
@@ -1,10 +1,8 @@
-package com.example.smile_hackathon_product.ui.exercise
+package com.example.smile_hackathon_product
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import com.example.smile_hackathon_product.R
+
 
 class ExerciseActivity : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/com/example/smile_hackathon_product/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/smile_hackathon_product/ui/home/HomeFragment.kt
@@ -1,10 +1,7 @@
 package com.example.smile_hackathon_product.ui.home
 
 import android.content.Intent
-import android.media.Image
 import android.os.Bundle
-import android.text.TextUtils.replace
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -13,10 +10,8 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import com.example.smile_hackathon_product.MainActivity
 import com.example.smile_hackathon_product.R
-import com.example.smile_hackathon_product.ui.dashboard.DashboardFragment
-import com.example.smile_hackathon_product.ui.exercise.ExerciseActivity
+import com.example.smile_hackathon_product.ExerciseActivity
 
 class HomeFragment : Fragment() {
 
@@ -36,20 +31,16 @@ class HomeFragment : Fragment() {
             textView.text = it
         })
 
-        // Viewの取得
-        var suquwatto: ImageButton = root.findViewById(R.id.suquwatto)
-        suquwatto.setOnClickListener{
-            val intent = Intent(activity, ExerciseActivity::class.java)
-            startActivity(intent)
+
+        //Viewの取得
+        var squat: ImageButton = root.findViewById(R.id.squat)
+
+        //ボタン押下時の動作
+        squat.setOnClickListener{
+                var intent = Intent(activity, ExerciseActivity::class.java)
+                startActivity(intent)
         }
 
         return root
-    }
-
-    // ボタン押下時の処理
-    public fun onButtonClick(view: TextView){
-        Log.i("NewItemFragment","onButtonClick")
-        // TextViewを書き換える
-        view.text = "BUTTONが押された"
     }
 }

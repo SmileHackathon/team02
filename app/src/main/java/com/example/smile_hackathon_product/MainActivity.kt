972e2bf5103f3514c754f8de9980fe270a652abc
funera1 <funeral7776@gmail.com>
2021-12-15 23:01:08 +0900

    [add]assetsからjsonを読み書きする

--- app/src/main/java/com/example/smile_hackathon_product/MainActivity.kt
+++ app/src/main/java/com/example/smile_hackathon_product/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.example.smile_hackathon_product
 
 import android.content.Intent
+import android.content.res.AssetManager
 import android.os.Bundle
+import android.renderscript.ScriptGroup
+import android.util.Log
 import android.widget.ImageButton
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
@@ -10,6 +13,8 @@ import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.smile_hackathon_product.ui.dashboard.DashboardFragment
+import org.json.JSONObject
+import java.io.*
 
 
 class MainActivity : AppCompatActivity() {
@@ -27,7 +32,32 @@ class MainActivity : AppCompatActivity() {
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
 
-
-
+        val jsonobj = readJson("Sample.json")
+//        print(jsonobj.get("sample"))
+    }
+    fun readJson(path: String) : JSONObject{
+//        val assetManager = resources.assets
+        var inputStream: InputStream? = null
+        try{
+            inputStream = getAssets().open(path)
+            val jsonString = inputStream.bufferedReader().use { it.readText() }
+            val jsonObject = JSONObject(jsonString)
+            return jsonObject
+        } catch (e: IOException) {
+            Log.d("AppErr", "File openning failed")
+        } finally {
+            try {
+                if (inputStream != null) inputStream.close()
+            } catch (e: IOException) {
+                Log.d("AppErr", "File closing failed")
+            }
+        }
+        val js = JSONObject("{}")
+        return js
+//        val jsonArray = jsonObject.getJSONArray(())
+//        val bufferedReader = BufferedReader(InputStreamReader(inputStream))
+//        val str: String = bufferedReader.readText()
+//        val jsonObject = JSONObject(str)
+//        return jsonObject
     }
 }
